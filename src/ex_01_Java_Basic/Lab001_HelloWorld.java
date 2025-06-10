84d65140982025d09e0f3fae6a0936628a919498
Punarva611 <cpunarva611@gmail.com>
2025-06-10 17:13:45 +0530

    Commit second update

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -3,6 +3,7 @@ package ex_01_Java_Basic;
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first lab homework");
+        System.out.println("Hello I like Java class");
     }
 
 
